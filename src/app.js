83d1acaa35cf302ec64afa9ac54600ded29ebd28
Vishal Gupta <guptaaryan1103@gmail.com>
2026-01-18 11:18:19 +0530

    make the signin API successfully

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const connectDB = require("./config/database");
-const User = require("./models/User")
+const User = require("./models/User");
 const app = express();
 const PORT = 4000;
 
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const user = new User({
-        firstName: "Aryan",
-        lastName: "Gupta",
-        emailId: "aryan@gmail.com",
-        password: "aryan123@"
-    })
+  const user = new User(req.body);
 
+  try {
     await user.save();
-    res.send("User added successfully!")
-})
-
-
+    res.send("User added successfully!");
+  } catch (err) {
+    res.status(400).send("Error saving the user: ", err.message);
+  }
+});
 
 connectDB()
   .then(() => {
