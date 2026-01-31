cbce0eb6eb34370d486f9d951e30de9917ee965b
Vishal Gupta <guptaaryan1103@gmail.com>
2026-01-31 12:43:38 +0530

    Set limit & skip function in feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -53,6 +53,12 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
@@ -69,9 +75,12 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(handleUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
 
-    res.send(users);
+    res.json({ data: users });
   } catch (err) {
     return res.status(401).json({ message: `Error: ${err.message}` });
   }
